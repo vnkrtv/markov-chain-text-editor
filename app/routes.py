6db5917680a98e1dc998b0520054637eb0d8ff80
LeadNess <leadness@mail.ru>
2020-07-15 01:50:22 +0300

    Update markov model class
    Fix model training - update __update fun
    Add save/load methods for working with model

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,17 @@ from app import forms
 from flask import render_template
 from flask.views import MethodView
 from googletrans import Translator
-from models.model import interact_model
+from models.markov.markov_model import MarkovModel
+from models.utils.postgres import PostgresStorage
+# from models.gpt2.generate_samples import interact_model
+
+storage = PostgresStorage.connect(host='172.17.0.2')
+texts_list = storage.get_posts_texts()
+print('Loaded texts: ', len(texts_list))
+markov_model = MarkovModel(order=5)
+markov_model.train(texts_list)
+print('Marcov model len: ', len(markov_model.model))
+markov_model.save()
 
 
 class ArticlesGenAPI(MethodView):
@@ -12,6 +22,8 @@ class ArticlesGenAPI(MethodView):
         'title': 'Генератор определений',
     }
     translator = Translator()
+    # storage = PostgresStorage.connect(host='172.17.0.2')
+    # texts_list = storage.get_posts_texts()
 
     def get(self):
         self.context['form'] = forms.PhraseForm()
@@ -22,19 +34,23 @@ class ArticlesGenAPI(MethodView):
     def post(self):
         form = forms.PhraseForm()
         if form.validate_on_submit():
-            self.context['message'] = form.phrase.data
-        else:
-            self.context['message'] = 'blya'
-        self.context['form'] = form
-        phrase = self.translator.translate(form.phrase.data).text
-        self.context['text_samples'] = [
-            form.phrase.data + ' ' + self.translator.translate(text, dest='ru').text
-            for text in interact_model(
-                phrase,
-                nsamples=form.samples_count.data,
-                top_k=20,
-                temperature=0.7)
-        ]
+            self.context['form'] = form
+            phrase = form.phrase.data
+            '''
+            phrase = self.translator.translate(form.phrase.data).text
+            self.context['text_samples'] = [
+                form.phrase.data + ' ' + self.translator.translate(text, dest='ru').text
+                for text in interact_model(
+                    phrase,
+                    nsamples=form.samples_count.data,
+                    top_k=20,
+                    temperature=0.7)
+            ]
+            '''
+            self.context['text_samples'] = [
+                markov_model.generate_by_phrase(30, phrase)
+                for _ in range(form.samples_count.data)
+            ]
         return render_template(self.template, **self.context)
 
 
--- models/markov/markov_model.py
+++ models/markov/markov_model.py
@@ -1,5 +1,6 @@
 import re
 import random
+import pickle
 from models.markov.histograms import Dictogram
 from models.markov.text_processor import TextProcessor
 
@@ -8,23 +9,91 @@ class MarkovModel:
 
     model: dict
     order: int
+    start_words: list
+    start_phrases: list
 
     def __init__(self, order=1):
         self.model = dict()
         self.order = order
+        self.start_words = []
+        self.start_phrases = []
 
     def train(self, texts_list: list):
         contain_rus = re.compile(r'[а-яА-Я]')
         for text in texts_list:
             if contain_rus.search(text):
+                text_words_list = []
                 for sentence in TextProcessor.tokenize(text):
                     words_list = sentence.split()
+                    if len(words_list) >= self.order:
+                        self.start_phrases.append(tuple(words_list[:self.order]))
+                        self.start_words.append(words_list[0])
                     words_list[-1] = words_list[-1][:-1]
-                    if len(words_list) > 1:
-                        self.update(words_list)
+                    text_words_list += words_list
+                self.__update(text_words_list)
 
-    def update(self, words_list: list):
-        for i in range(0, len(self.model) - self.order):
+    def generate_by_word(self, length, start_word=None):
+        if not start_word:
+            start_word = self.__generate_start_word()
+        sentence = [start_word]
+
+        samples_list = []
+        for phrase in self.model:
+            if phrase[0] == start_word:
+                samples_list.append(phrase)
+        current_words = random.choice(samples_list)
+
+        for _ in range(1, length):
+            current_dictogram = self.model[current_words]
+            random_weighted_words = current_dictogram.return_weighted_random_word()
+            current_words = random_weighted_words
+            sentence.append(current_words)
+
+        sentence[0] = sentence[0].capitalize()
+        return ' '.join(sentence) + '.'
+
+    def generate_by_phrase(self, length, start_phrase=None):
+        if not start_phrase:
+            start_phrase = self.__generate_start_phrase()
+        sentence = start_phrase.split()
+
+        samples_list = []
+        for phrase in self.model:
+            if phrase[:len(sentence)] == sentence:
+                samples_list.append(phrase)
+        current_words = random.choice(samples_list)
+
+        for _ in range(len(sentence), length):
+            current_dictogram = self.model[current_words]
+            random_weighted_words = current_dictogram.return_weighted_random_word()
+            current_words = random_weighted_words
+            sentence.append(current_words)
+
+        sentence[0] = sentence[0].capitalize()
+        return ' '.join(sentence) + '.'
+
+    def save(self, path='app/models/markov/'):
+        with open(path + 'model.pickle', 'wb') as handle:
+            pickle.dump(self.model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(path + 'order.pickle', 'wb') as handle:
+            pickle.dump(self.order, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(path + 'start_words.pickle', 'wb') as handle:
+            pickle.dump(self.start_words, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(path + 'start_phrases.pickle', 'wb') as handle:
+            pickle.dump(self.start_phrases, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def load(self, path='app/models/markov/'):
+        with open(path + 'model.pickle', 'rb') as handle:
+            self.model = pickle.load(handle)
+        with open(path + 'order.pickle', 'rb') as handle:
+            self.order = pickle.load(handle)
+        with open(path + 'start_words.pickle', 'rb') as handle:
+            self.start_words = pickle.load(handle)
+        with open(path + 'start_phrases.pickle', 'rb') as handle:
+            self.start_phrases = pickle.load(handle)
+
+    def __update(self, words_list: list):
+        for i in range(0, len(words_list) - self.order):
             # Создаем окно
             window = tuple(self.model[i: i + self.order])
             # Добавляем в словарь
@@ -34,16 +103,8 @@ class MarkovModel:
             else:
                 self.model[window] = Dictogram([words_list[i + self.order]])
 
-    def generate_random_start(self):
-        key = random.choice(list(self.model.keys()))
-        # Чтобы сгенерировать любое начальное слово, раскомментируйте строку:
-        # return random.choice(gpt-2.keys())
+    def __generate_start_word(self):
+        return random.choice(list(self.start_words))
 
-        # Чтобы сгенерировать "правильное" начальное слово, используйте код ниже:
-        # Правильные начальные слова - это те, что являлись началом предложений в корпусе
-        if 'END' in model:
-            seed_word = 'END'
-            while seed_word == 'END':
-                seed_word = model['END'].return_weighted_random_word()
-            return seed_word
-        return random.choice(list(model.keys()))
+    def __generate_start_phrase(self):
+        return random.choice(list(self.start_phrases))
