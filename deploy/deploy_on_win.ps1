ca1569d61bd02c84a3b8f917930a5532c3094cde
LeadNess <leadness@mail.ru>
2020-10-02 01:34:27 +0300

    Add deploying script
    Add script for deploying app on windows

---
+++ deploy/deploy_on_win.ps1
@@ -0,0 +1,15 @@
+python -m venv venv
+Write-Host "Created virtual environment..."
+.\venv\Scripts\activate
+
+Write-Host "Start loading requirements..."
+pip install --no-cache-dir -r requirements.txt
+Write-Host "Successfully loaded requeirments..."
+
+$env:FLASK_APP=wsgi.py
+flask db init
+flask db migrate
+flask db upgrade
+Write-Host "Applied database migrations..."
+
+Write-Host "Run app with: gunicorn wsgi:app -b 0.0.0.0:5000"
